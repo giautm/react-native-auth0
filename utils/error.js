46cdcaed9c46b592127979fbecf7a014258dc4ed
giautm <giau.tran@tiki.vn>
2017-01-21 18:37:46 +0700

    fix core-bug

--- utils/error.js
+++ utils/error.js
@@ -1,9 +1,9 @@
 export default class Auth0Error extends Error {
-  constructor (json) {
+  constructor (json, response) {
     super();
     Error.captureStackTrace(this, this.constructor);
     this.name = json.error || json.code || 'a0.internal.failed';
-    this.message = json.description || json.error_description;
+    this.message = json.description || json.error_description || response.statusText || response.status;
     this.json = json;
   }
 };
